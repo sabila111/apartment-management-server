258609fd38829a0ff775a60310c0d4ba282b1d30
sabila111 <sharminsharker2002@gmail.com>
2025-01-26 19:40:34 +0600

    installed jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
